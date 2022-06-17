1f5e86602d84ba3fd8788ca5473034f09011aa6b
Ader <40139500+AderitoSilva@users.noreply.github.com>
2022-06-17 20:21:39 +0100

    Added future considerations to the TODO

--- source/XInputium/XInputium/_TODO.cs
+++ source/XInputium/XInputium/_TODO.cs
@@ -1,4 +1,5 @@
 ﻿// This file contains TODO comments.
 
 // TODO Implement a Joystick property that returns the effective joystick quadrant (ex. TopLeft, Left, Top, etc.).
+// TODO To consider: A property in XGamepad class that accepts a ModifierFunction that modifies vibration at the logical device level.
 
