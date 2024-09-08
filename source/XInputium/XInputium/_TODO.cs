54d661efdf5c48aac6fb93168d3802aea53355c9
Pespego <40139500+AderitoSilva@users.noreply.github.com>
2024-09-08 01:16:47 +0100

    Added a few TODOs

--- source/XInputium/XInputium/_TODO.cs
+++ source/XInputium/XInputium/_TODO.cs
@@ -1,4 +1,6 @@
 ﻿// This file contains TODO comments.
 
 // TODO To consider: A property in XGamepad class that accepts a ModifierFunction that modifies vibration at the logical device's scope.
+// TODO Update `Joystick`'s and `Trigger`'s smoothing algorithm to use a consumer provided algorithm, and implement IIR (Infinite Impulse Response) as the default algorithm.
+// TODO Implement an all-purpose thread-safe, high precision input loop.
 
